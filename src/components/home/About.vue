07b494b30f6c1b70a049bb2eb34bde1775e1c183
BENZABOSS <joyceben33@gmail.com>
2024-07-07 15:14:41 -0400

    change about me image

--- src/components/home/About.vue
+++ src/components/home/About.vue
@@ -1,47 +1,39 @@
 <template>
-    <section id="about-me">
-        <v-container>
-            <v-row>
-                <v-col class="text-center" cols="12" md="12" lg="12" xl="12">
-                    <h2>About Me</h2>
-                </v-col>
-            </v-row>
-            <v-row justify="center" align="center">
-                <v-col cols="12" md="7">
-                    <p>
-                        I'm a Full Stack Developer with over 3 years of programming experience, having worked on
-                        multiple production
-                        grade applications in an Agile setting. I've collaborated with UI/UX designers, Product
-                        Owners,
-                        and other
-                        engineers to ensure I'm delivering software that the customer needs or patching critical bug
-                        fixes in a
-                        timely manner. I consider myself as a supportive and enthusiastic team player, dedicated to
-                        streamlining
-                        development processes. My mantra is “Work smarter not harder”. Outside of work I enjoy
-                        hitting
-                        tennis,
-                        weight training, & watching / attending NC State Athletic Events.
-                    </p>
-                </v-col>
-                <v-col cols="12" md="5">
-                    <v-img id="about-profile-img" :src="ProfileImage" width="300" ></v-img>
-                </v-col>
-            </v-row>
-        </v-container>
-    </section>
+  <section id="about-me">
+    <v-container>
+      <v-row>
+        <v-col class="text-center" cols="12" md="12" lg="12" xl="12">
+          <h2>About Me</h2>
+        </v-col>
+      </v-row>
+      <v-row justify="center" align="center">
+        <v-col cols="12" md="7">
+          <p>
+            I'm a Full Stack Developer with over 3 years of programming experience, having worked on multiple production
+            grade applications in an Agile setting. I've collaborated with UI/UX designers, Product Owners, and other
+            engineers to ensure I'm delivering software that the customer needs or patching critical bug fixes in a
+            timely manner. I consider myself as a supportive and enthusiastic team player, dedicated to streamlining
+            development processes. My mantra is “Work smarter not harder”. Outside of work I enjoy hitting tennis,
+            weight training, & watching / attending NC State Athletic Events.
+          </p>
+        </v-col>
+        <v-col cols="12" md="5">
+          <v-img id="about-profile-img" :src="AboutMeImage" width="300"></v-img>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
 </template>
 <script setup lang="ts">
-import ProfileImage from '@/assets/profile_black_and_white.jpg'
-
+import AboutMeImage from '@/assets/nc_state_final_four_2024.jpeg';
 </script>
 <style lang="scss" scoped>
 #about-profile-img {
-    border-radius: 50%;
+  border-radius: 50%;
 }
 
 .myBio {
-    min-width: 380px;
-    max-width: 750px;
+  min-width: 380px;
+  max-width: 750px;
 }
 </style>
